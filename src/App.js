dddddf6b52b2c7daa3b6f0c63a3a79d8b97bb2a1
risarisato <nqg44226@gmail.com>
2024-06-17 02:21:41 +0900

    3つのAPIから取得

--- src/App.js
+++ src/App.js
@@ -3,28 +3,34 @@ import './App.css';
 import Card from "./components/Card/Card";
 import ImageCard from "./components/ImageCard/ImageCard";
 import Navbar from "./components/Navber/Navbar";
+import HarryPotterCard from "./components/HarryPotter/HarryPotterCard";
 import { getAllPokemon, getPokemon } from "./utiles/pokemon.js";
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
   const imagesURL = "https://picsum.photos/v2/list?page=2&limit=20";
+  const harryPotterURL = "https://hp-api.onrender.com/api/characters";
   const [loading, setLoading] = useState(true);
   const [pokemonData, setPokemonData] = useState([]);
   const [images, setImages] = useState([]);
+  const [harryPotterData, setHarryPotterData] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const [pokemonRes, imagesRes] = await Promise.all([
+        const [pokemonRes, imagesRes, harryPotterRes] = await Promise.all([
           getAllPokemon(initialURL),
-          fetch(imagesURL)
+          fetch(imagesURL),
+          fetch(harryPotterURL)
         ]);
 
         const pokemonData = await loadPokemon(pokemonRes.results);
         const imagesData = await imagesRes.json();
+        const harryPotterData = await harryPotterRes.json();
 
         setPokemonData(pokemonData);
         setImages(imagesData);
+        setHarryPotterData(harryPotterData);
       } catch (error) {
         console.error("データの取得に失敗しました", error);
       } finally {
@@ -50,14 +56,19 @@ function App() {
           <h1>Loading...ロード中です。</h1>
         ) : (
           <div className="container">
-            <div className="pokemonCardContainer">
-              {pokemonData.map((pokemon, i) => (
-                <Card key={i} pokemon={pokemon} />
-              ))}
-            </div>
             <div className="imageContainer">
               {images.map((image, index) => (
                 <ImageCard key={index} image={image} />
+              ))}
+            </div>
+            <div className="harryPotterContainer">
+              {harryPotterData.map((character, index) => (
+                <HarryPotterCard key={index} character={character} />
+              ))}
+            </div>
+            <div className="pokemonCardContainer">
+              {pokemonData.map((pokemon, i) => (
+                <Card key={i} pokemon={pokemon} />
               ))}
             </div>
           </div>
---
+++ src/components/HarryPotter/HarryPotterCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./HarryPotterCard.css";
+
+const HarryPotterCard = ({ character }) => {
+  return (
+    <div className="harryPotterCard">
+      <img src={character.image} />
+      <h3>{character.name}</h3>
+      <p>{character.dateOfBirth}</p>
+    </div>
+  );
+};
+
+export default HarryPotterCard;
