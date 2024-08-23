d4aa69073b56cc7b29937f48bc0dbec249916554
risarisato <nqg44226@gmail.com>
2024-08-23 17:25:18 +0900

    type3

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import './App.css';
 import Card from "./components/pokemonCard/pokemonCard";
 import ImageCard from "./components/ImageCard/ImageCard";
-import Navbar from "./components/Navber/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import HarryPotterCard from "./components/HarryPotter/HarryPotterCard";
 import { getAllPokemon, getPokemon } from "./utiles/pokemon.js";
 
