44b642e55744fc2f09d4d319a163789509561d08
risarisato <nqg44226@gmail.com>
2024-08-23 17:13:24 +0900

    type修正

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+    return (
+        <nav>
+            {/* <h1>PokemonAPI</h1> */}
+            <h1>3APIs_components_fetchDatas_tutorial</h1>
+            <img src="/logo192.png" />
+        </nav>
+    );
+};
+
+export default Navbar;
